1c4c97ff4360a20485a415f1af7225dbcc8ba276
FranciscoRT-2399 <flash2_@hotmail.com>
2024-03-18 01:22:56 -0600

    Se añadió compilación a Js

--- gulpfile.js
+++ gulpfile.js
@@ -55,19 +55,29 @@ function versionAvif(done){
   };
   src("src/img/**/*{png,jpg}")
   .pipe(avif(opciones))
-  .pipe(dest("build/img"))
+  .pipe(dest("build/img"));
 
   done(); //Callback
 }
 
+function javascript(done){
+  src("src/js/**/*.js")
+  .pipe(dest("build/js"));
+
+  done(); //Callback
+} 
 
 
 function dev(done){
-  watch("src/scss/**/*.scss", css) /* 'ast ast / ast' detecta todos los aravicos .scss */
+  watch("src/scss/**/*.scss", css); /* 'ast ast / ast' detecta todos los aravicos .scss */
+  watch("src/js/**/*.js", javascript);
+
+  done(); //Callback
 }
 
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
